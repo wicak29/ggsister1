5f8d884cd64748e62392333c429400b76f384e2b
wicak29 <adi.wicaksana13@mhs.if.its.ac.id>
2016-03-10 07:19:43 +0700

    modification code

--- RPC/client.py
+++ RPC/client.py
@@ -1,8 +1,82 @@
 import xmlrpc.client
 import sys
+#import time
 
-s = xmlrpc.client.ServerProxy('http://localhost:8000')
-s.parseFile('messages')
+import glob
+import os
+import threading
+import timeit
+from collections import OrderedDict
 
+array = []
+proxy = []
+eventLogs = []
+events = {}
+counter = 0
+
+#s = xmlrpc.client.ServerProxy('http://localhost:8000')
+#start = time.time()
+#s.parseFile('messages.1')
+
+#-------------------------------------------------------------------------
+
+def parsing(worker,file):
+	fileobj = open(file)
+	print (fileobj)
+	eventLogs.append(worker.line_counter(fileobj))
+
+# menghitung worker yang ada
+workers=0
+with open ("workers.txt", "r") as wrk:
+	for line in wrk:
+		array.append(line)
+		workers=workers+1
+
+for line in array:
+	print ("http://"+line.rstrip('\n').split(';',1)[0])
+	worker = xmlrpc.client.ServerProxy("http://"+line.rstrip('\n').split(';',1)[0], allow_none=True)
+	print(worker.add(2,3))  # Returns 5
+	# worker = xmlrpc.client.ServerProxy(line)	
+	proxy.append(worker)
+
+
+i=0
+os.chdir("log")
+start = timeit.default_timer()
+threads = []
+for file in glob.glob("*.*"):
+	fileobj = open(file)
+	# linecount = worker.line_counter(fileobj)
+	# eventLogs.append(proxy[i%workers].line_counter(fileobj))
+	# print (linecount)
+	# t = threading.Thread(target=parsing,args=(proxy[i%workers],file,));
+	# t.start()
+	# threads.append(t)
+	i=i+1
+
+for thread in threads:
+	thread.join()
+
+for event in eventLogs:
+	for key, value in event:
+		if key in events:
+			events[key] = events[key] + value
+		else:
+			events[key] = value
+
+stop = timeit.default_timer()
+print (" Waktu komputasi yang dibutuhkan : ", (stop - start)) 
+
+flag = 1
+topTen = sorted(events.items(), key = lambda t:t[1], reverse=True)
+for key, value in topTen:
+	print (key,' ',value)
+	flag+=1
+	if (flag > 10):
+		break;
+#---------------------------------------------------------------------------
 # Print list of available methods
-print(s.system.listMethods())
+#print(s.system.listMethods())
+
+# print time spent
+#print("%s seconds" % (time.time() - start))
--- RPC/server.py
+++ RPC/server.py
@@ -1,72 +1,30 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
-import sys
-
-# Restrict to a particular path.
-class RequestHandler(SimpleXMLRPCRequestHandler):
-    rpc_paths = ('/RPC2',)
+from customParser import parseObject
+import datetime
+import glob
+import os
+import timeit
+import threading
+import socket
+import traceback
+import pickle
+#import sys
 
 # Create server
-server = SimpleXMLRPCServer(("localhost", 8000),
-                            requestHandler=RequestHandler)
-server.register_introspection_functions()
+server = SimpleXMLRPCServer(("localhost", 8000), logRequests=True, allow_none=True)
+# server.register_introspection_functions()
 print("Listening on port 8000...")
 
-#inisialisasi pemisah kata dan karakter
-stopWord = ['for', 'from', 'to', 'with', 'after', 'at']
-forbiddenChar = [';', ':', ',', '(']
-
-#definisi fungsi
-def parseLine(lineText):
-	logDetails = lineText.split()[5:]
-	eventName = ''
-	for word in logDetails:
-		
-		if (word in stopWord):
-			break;
-
-		if ('(' in word):
-			stopChar = word.index('(')
-			if stopChar != 0:
-				eventName = eventName + ' ' + word[0:stopChar]
-			break;
-
-		if (';' in word) or (':' in word) or (',' in word):
-			word = word[:-1]
-			eventName = eventName + ' ' + word
-			break;
-
-		if "'" in word:
-			break;
-
-		eventName = eventName + ' ' + word
-
-	return eventName
-
-def parseFile(logFile):
-	eventLog = {}
-
-	with open(logFile) as log:
-		for line in log:
-			eventName = parseLine(line);
-			if eventName in eventLog:
-				eventLog[eventName] = eventLog[eventName] + 1
-			else:
-				eventLog[eventName] = 1
-
-	eventLog = sortByValue(eventLog)
-	for key, value in eventLog:
-		print (key, value)
-
+# Fungsi line counter
+def line_counter(fileobj):
+	eventLog = parseObject(fileobj)
 	return eventLog
 
-def sortByValue(dict):
-	result = sorted(dict.items(), key = lambda t:t[1])
-	return result
+def adder_function(x,y):
+    return x + y
+server.register_function(adder_function, 'add')
 
-server.register_function(parseLine, 'parseLine')
-server.register_function(parseFile, 'parseFile')
-server.register_function(sortByValue, 'sortByValue')
-
+server.register_function(line_counter, 'line_counter')
 # Run the server's main loop
 server.serve_forever()
