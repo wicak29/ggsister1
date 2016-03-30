5d070ad39b1016e00b69e765c60d3d7c35a88c0b
daniel fablius <danielfablius@gmail.com>
2016-03-30 22:52:54 +0700

    MQ->FileOnManager

--- MQ/manager_fileOnManager.py
+++ MQ/manager_fileOnManager.py
@@ -1,1 +1,72 @@
 #manager file - file On Manager
+#!/usr/bin/env python
+import pika
+import uuid
+import os
+import glob
+import json
+import timeit
+
+eventLogs = []
+events = {}
+class MessageParserWorker(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True,
+                                   queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = json.loads(body)
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return self.response
+
+ParserWorker = MessageParserWorker()
+
+start = timeit.default_timer()
+
+os.chdir("log")
+
+for file in glob.glob("*.*"):
+    n = open(file,'r').read()
+    # print(" [x] fib(30)")
+    eventLogs.append(ParserWorker.call(n))
+
+for event in eventLogs:
+    for key, value in event:
+        if key in events:
+            events[key] = events[key] + value
+        else:
+            events[key] = value
+
+stop = timeit.default_timer()
+print " Waktu komputasi yang dibutuhkan : ", (stop - start)
+
+flag = 1
+topTen = sorted(events.items(), key = lambda t:t[1], reverse=True)
+for key, value in topTen:
+    print key,' ',value
+    flag+=1
+    if (flag > 10):
+        break;
+
+
--- MQ/worker_fileOnManager.py
+++ MQ/worker_fileOnManager.py
@@ -1,1 +1,36 @@
 #worker file - file On Manager
+#!/usr/bin/env python
+from customParser import parseText
+import pika
+import json
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+
+channel = connection.channel()
+
+channel.queue_declare(queue='rpc_queue')
+
+
+def on_request(ch, method, props, body):
+    n = body
+
+    response = parseText(n)
+    print response
+
+    print(" --- Send result back to the manager --- ")
+    ch.basic_publish(exchange='',
+                     routing_key=props.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         props.correlation_id),
+                     body=json.dumps(response))
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+    print(" --- Finish processing request --- ")
+
+    print(" [x] Awaiting requests")
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
