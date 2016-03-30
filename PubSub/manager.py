19405bddb8e4a5a1f47b4da9cafb7f9163904fa8
wicak29 <adi.wicaksana13@mhs.if.its.ac.id>
2016-03-31 04:27:26 +0700

    masih salah pas kirim balik

--- PubSub/manager.py
+++ PubSub/manager.py
@@ -1,1 +1,55 @@
-#manager file - Publish Subscribe
+#!/usr/bin/env python
+import pika
+import sys
+import os
+import timeit
+import glob
+
+os.chdir("log")
+
+eventLogs = []
+events = {}
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         type='fanout')
+
+# result = channel.queue_declare(exclusive=True)
+# callback_queue = result.method.queue
+# channel.basic_consume(on_response, no_ack=True, queue=callback_queue)
+
+start = timeit.default_timer()
+
+for fileName in glob.glob("*.*"):
+    print(" [x] Memproses file . . .")
+    message = fileName
+    eventLogs.append(fileName)
+    #message = ' '.join(sys.argv[1:]) or "info: Hello World!"
+    channel.basic_publish(exchange='logs',
+	                      routing_key='',
+	                      body=message)
+#print(" [x] Sent %r" % message)
+
+for event in eventLogs:
+    for key, value in event:
+        if key in events:
+            events[key] = events[key] + value
+        else:
+            events[key] = value
+
+stop = timeit.default_timer()
+print " Waktu komputasi yang dibutuhkan : ", (stop - start)
+
+flag = 1
+topTen = sorted(events.items(), key = lambda t:t[1], reverse=True)
+for key, value in topTen:
+    print key,' ',value
+    flag+=1
+    if (flag > 10):
+        break;
+
+
+connection.close()
--- PubSub/worker.py
+++ PubSub/worker.py
@@ -1,1 +1,47 @@
-#worker file - Publish Subscribe
+#!/usr/bin/env python
+import pika
+import os
+import glob
+import json
+from customParser import parseText
+
+os.chdir("log")
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs',
+                         type='fanout')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='logs',
+                   queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+	print(" --- Receive request --- ")
+	fileName = body
+	message = open(fileName, 'r').read()
+	response = parseText(message)
+	print response
+
+	ch.basic_publish(exchange='',
+                     routing_key=properties.reply_to,
+                     properties=pika.BasicProperties(correlation_id = \
+                                                         properties.correlation_id),
+                     body=json.dumps(response))
+	ch.basic_ack(delivery_tag = method.delivery_tag)
+	print(" --- Finish processing request --- ")
+
+    #-----------------------------------------
+    #print(" [x] %r" % body)
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+
+channel.start_consuming()
