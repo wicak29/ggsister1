4822e85ee90905ae9a751c467e4b81af05850e89
wicak29 <adi.wicaksana13@mhs.if.its.ac.id>
2016-05-06 08:44:30 +0700

    sudah mengeluarkan hasil, tapi ada keanehan

--- Clustering/manager.py
+++ Clustering/manager.py
@@ -1,5 +1,48 @@
 from tasks import add
+from tasks import gen_prime
+from tasks import messageParse
+import os
+import json
+import glob
+import timeit
+import threading
+import time
+
+eventLogs = []
+events = {}
+threads = []
+
+i = 0
+msg = ''
+start = timeit.default_timer()
+
+for file in glob.glob("*"):
+    n = open(file, 'r').read()
+    msg = messageParse(str(n))
+    eventLogs.append(msg)
+
+print "MSG : ", msg
+
+for event in eventLogs:
+    for key, value in event:
+        if key in events:
+            events[key] = events[key] + value
+        else:
+            events[key] = value
+
+stop = timeit.default_timer()
+print " Waktu komputasi yang dibutuhkan : ", (stop - start)
+
+flag = 1
+topTen = sorted(events.items(), key = lambda t:t[1], reverse=True)
+for key, value in topTen:
+    print key,' ',value
+    flag+=1
+    if (flag > 10):
+        break;
 
 result = add.delay(4, 4)
+prima = gen_prime.delay(100)
 
 print result.get()
+print prima.get()
--- Clustering/tasks.py
+++ Clustering/tasks.py
@@ -1,7 +1,38 @@
 from celery import Celery
+from customParser import parseText
+import timeit
+import os
+import glob
+import json
 
 app = Celery('tasks', backend='rpc://', broker='amqp://guest@localhost//')
+
+os.chdir("log/")
+eventLogs = []
+events = {}
 
 @app.task
 def add(x, y):
     return x + y
+
+@app.task(ignore_result=True)
+def print_hello():
+    print 'hello there'
+
+@app.task
+def gen_prime(x):
+    multiples = []
+    results = []
+    for i in xrange(2, x+1):
+        if i not in multiples:
+            results.append(i)
+            for j in xrange(i*i, x+1, i):
+                multiples.append(j)
+    return results
+
+@app.task
+def messageParse(file):
+    hasil = file
+    response = parseText(hasil)
+    print hasil
+    return response
